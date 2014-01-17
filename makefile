80c2a84dc572b60248aa02d24a81679edadc8a6d
David Selassie <selassid@gmail.com>
2014-01-17 15:51:43 -0800

    Fixes manifest for text files.

--- makefile
+++ makefile
@@ -11,3 +11,5 @@ clean:
 	find . -name '*.pyc' -delete
 	find . -name '__pycache__' -delete
 	rm -rf .tox
+	rm -rf canopener.egg-info
+	rm -rf dist
--- setup.py
+++ setup.py
@@ -2,21 +2,23 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
+from pkg_resources import resource_string
 from setuptools import setup
 
 
 setup(
     name='canopener',
-    version='0.1.2',
+    version='0.1.3',
     author='David Selassie',
     author_email='selassid@gmail.com',
     package='canopener',
     url='https://github.com/selassid/canopener',
-    license='LICENSE.txt',
+    license=resource_string(__name__, 'LICENSE.txt'),
     description=(
         'Python convenience function for opening compressed URLs and files.'
     ),
-    long_description=open('README.rst').read(),
+    include_package_data=True,
+    long_description=resource_string(__name__, 'README.rst'),
     setup_requires=['setuptools'],
     install_requires=[
         'boto',
