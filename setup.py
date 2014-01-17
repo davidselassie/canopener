37f71ed56c3a96ca71f8200bc8fc8cfda900a85d
David Selassie <selassid@gmail.com>
2014-01-16 18:14:21 -0800

    Bumps version.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name='canopener',
-    version='0.1.1',
+    version='0.1.2',
     author='David Selassie',
     author_email='selassid@gmail.com',
     package='canopener',
