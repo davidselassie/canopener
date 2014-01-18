967d61b74cac1981f84c28291d40b341b945f486
David Selassie <selassid@gmail.com>
2014-01-17 16:13:34 -0800

    Uses find_packages.

--- setup.py
+++ setup.py
@@ -3,15 +3,16 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 
 from pkg_resources import resource_string
+from setuptools import find_packages
 from setuptools import setup
 
 
 setup(
     name='canopener',
-    version='0.1.3',
+    version='0.1.4',
     author='David Selassie',
     author_email='selassid@gmail.com',
-    package='canopener',
+    packages=find_packages(exclude=['tests']),
     url='https://github.com/selassid/canopener',
     license=resource_string(__name__, 'LICENSE.txt'),
     description=(
