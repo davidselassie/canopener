a1e09f8da1b835c7a8543836fb3c23b50c1f1257
David Selassie <selassid@gmail.com>
2014-12-22 20:55:33 -0800

    Bumps version to 0.1.5.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='canopener',
-    version='0.1.4',
+    version='0.1.5',
     author='David Selassie',
     author_email='selassid@gmail.com',
     packages=find_packages(exclude=['tests']),
